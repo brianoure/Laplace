c7600f62ad9c2464247b5573e6b0b31287946805
brianoure <61065227+brianoure@users.noreply.github.com>
2021-06-28 14:17:24 +0300

    Create complex_number.cpp
    Calculate complex numbers

---
+++ complex_number.cpp
@@ -0,0 +1,9 @@
+struct B_COMPLEX{
+float real;
+float imaginary;
+}
+
+B_COMPLEX public addComplex(B_COMPLEX f, B_COMPLEX s)
+B_COMPLEX public subComplex(B_COMPLEX f, B_COMPLEX s)
+B_COMPLEX public prodComplex(B_COMPLEX f, B_COMPLEX s)
+B_COMPLEX public divComplex(B_COMPLEX f, B_COMPLEX s)
